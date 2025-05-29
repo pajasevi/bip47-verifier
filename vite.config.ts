c9c5aeca23dd121df76c502703c34f5e9b0b149a
Dojo Coder <dojocoder@pm.me>
2025-05-29 09:34:12 +0200

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import preact from '@preact/preset-vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "bip47-verifier",
+    base: "/bip47-verifier",
     plugins: [
         preact(),
     ],
